57a970f78648045cb80dcc0d64fc4f8ec1153a32
Hanai <yuuukariforev626111@gmail.com>
2026-07-29 16:14:53 +0900

    型注釈・型推論に関して

--- lesson/index.js
+++ lesson/index.js
@@ -1,5 +1,5 @@
+"use strict";
 // js にコンパイルする際は `tsc {ファイル名}`
 // Node.js 上で実行する際には、node {ファイル名}
-
-let hello : string   = 'Hello'
-console.log(hello)
+let hello = 'Hello';
+console.log(hello);
---
+++ type.ts
@@ -0,0 +1,21 @@
+// 基本的な型について
+
+let hasValue: boolean = true;
+let count: number = 3;
+let float: number = 3.14;
+let negative: number = -3.12;
+let single: string = 'hello';
+let double: string = "Hello";
+let back: string = `Hello2`;
+
+// 型注釈・型推論について
+// let hasValue2: boolean = true; :boolean が型注釈のこと
+// ここを記載しないと、型推論で初期値の値を元に型が決まってしまう
+
+let hasValue2: boolean = true;
+let count2: number = 3;
+let float2: number = 3.14;
+let negative2: number = -3.12;
+let single2: string = 'hello';
+let double2: string = "Hello";
+let back2: string = `Hello2`;
