28fcdc0f477ac31f2588d328ff430c5c464f9ea9
Hanai <yuuukariforev626111@gmail.com>
2026-08-13 08:49:59 +0900

    private public について記載

--- dist/class.js
+++ dist/class.js
@@ -2,15 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class Person {
     name;
+    age; // クラス外からはアクセスしないようにする
     // インスタンス生成時に最初に実行される関数
-    constructor(initName) {
+    constructor(initName, initAge) {
         this.name = initName;
+        this.age = initAge;
     }
-    // method を作成する
+    // 年齢を増加する
+    incrementAge() {
+        this.age++;
+    }
+    // method を作成する (メソッドに明示的に型を指定する)
     greeting() {
-        console.log(`Hello my name is ${this.name}`);
+        console.log(`Hello my name is ${this.name}. I am ${this.age} years old.`);
     }
 }
 // インスタンス生成
-const yuma = new Person("yuma");
+const yuma = new Person("yuma", 25);
+yuma.incrementAge();
 yuma.greeting();
--- src/class.ts
+++ src/class.ts
@@ -1,15 +1,24 @@
 class Person{
     name : string;
+    private age : number; // クラス外からはアクセスしないようにする（先頭に#をつけることによってprivateと同じにすることが可能）
+
     // インスタンス生成時に最初に実行される関数
-    constructor(initName : string){
+    constructor(initName : string , initAge : number){
         this.name = initName;
+        this.age = initAge;
+    }
+
+    // 年齢を増加する
+    incrementAge(){
+        this.age++
     }
 
     // method を作成する (メソッドに明示的に型を指定する)
     greeting(this :Person){
-        console.log(`Hello my name is ${this.name}`)
+        console.log(`Hello my name is ${this.name}. I am ${this.age} years old.`)
     }
 }
 // インスタンス生成
-const yuma = new Person("yuma");
+const yuma = new Person("yuma" , 25);
+yuma.incrementAge();
 yuma.greeting();
