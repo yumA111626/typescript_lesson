5d18f87399ef300513cd2349aae4b27c23b87ed1
Hanai <yuuukariforev626111@gmail.com>
2026-08-16 15:27:58 +0900

    readonly について記載

--- src/class.ts
+++ src/class.ts
@@ -1,11 +1,13 @@
 class Person{
-    name : string;
+    readonly name : string; // readonly と記載することによって、外部から書き換えをすることができなくなる
+    readonly id : number = 32; // readonly と記載することによって、外部から書き換えをすることができなくなる
     private age : number; // クラス外からはアクセスしないようにする（先頭に#をつけることによってprivateと同じにすることが可能）
 
     // インスタンス生成時に最初に実行される関数
-    constructor(initName : string , initAge : number){
+    constructor(readonly initName : string , initAge : number){
         this.name = initName;
         this.age = initAge;
+        this.id = 33; // コンストラクタ関数内では値の書き換えは実行可能
     }
 
     // 初期化の処理を省略して記載する場合
